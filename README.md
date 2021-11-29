1ae104d22aaa2cd3d50d63c0fc5d7207fd7d8357
Amir Hossein Roohi <ahroohi1379@hotmail.com>
2021-11-29 21:27:44 +0330

    added a better neofetch

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # dotfiles
 My dotfiles config.
 Nothing special really
+What can I say, I'm a man of culture
--- classes.py
+++ classes.py
@@ -3,26 +3,34 @@ import calendar
 from prettytable import PrettyTable
 
 today = calendar.day_name[date.today().weekday()]
-myTable = PrettyTable(["Class", "Time", "Link"])
+sa_table = PrettyTable(["Class", "Time", "Link"])
+su_table = PrettyTable(["Class", "Time", "Link"])
+mo_table = PrettyTable(["Class", "Time", "Link"])
+tu_table = PrettyTable(["Class", "Time", "Link"])
+we_table = PrettyTable(["Class", "Time", "Link"])
 
-if today == "Saturday":
-    myTable.add_row(["Statistic and Probability", "10 - 12", "https://www.skyroom.online/ch/shhut.ac.ir/shahsanaii"])
-    myTable.add_row(["Electrical Circuites", "12 - 14", "https://www.skyroom.online/ch/shhut.ac.ir/erfani"])
-    myTable.add_row(["Engineer Maths", "18 - 20", "https://www.skyroom.online/ch/shhut.ac.ir/chaji"])
-elif today == "Sunday":
-    myTable.add_row(["Electromangnatism", "16 - 18", "https://www.skyroom.online/ch/shhut.ac.ir/mazraeh-fard"])
-elif today == "Monday":
-    myTable.add_row(["Electrical Circuites", "12 - 14", "https://www.skyroom.online/ch/shhut.ac.ir/erfani"])
-    myTable.add_row(["Sport 1", "14 - 16", "https://www.skyroom.online/ch/shhut.ac.ir/darvakh"])
-    myTable.add_row(["Electrical Language", "18 - 20", "https://www.skyroom.online/ch/shhut.ac.ir/erfani"])
-elif today == "Tuesday":
-    myTable.add_row(["Engineer Maths", "08 - 10", "https://www.skyroom.online/ch/shhut.ac.ir/chaji"])
-    myTable.add_row(["Statistic and Probability", "10 - 12", "https://www.skyroom.online/ch/shhut.ac.ir/shahsanaii"])
-    myTable.add_row(["Islamic Revolution", "14 - 16", "https://www.skyroom.online/ch/shhut.ac.ir/darvakh"])
-    myTable.add_row(["Electromangnatism", "16 - 18", "https://www.skyroom.online/ch/shhut.ac.ir/mazraeh-fard"])
-elif today == "Wednesday":
-    myTable.add_row(["Number Calculations", "08 - 10", "https://www.skyroom.online/ch/shhut.ac.ir/pouyan"])
-    myTable.add_row(["Number Calculations", "10 - 12", "https://www.skyroom.online/ch/shhut.ac.ir/keramat"])
+sa_table.add_row(["Statistic and Probability", "10 - 12", "https://www.skyroom.online/ch/shhut.ac.ir/shahsanaii"])
+sa_table.add_row(["Electrical Circuites", "12 - 14", "https://www.skyroom.online/ch/shhut.ac.ir/erfani"])
+sa_table.add_row(["Engineer Maths", "18 - 20", "https://www.skyroom.online/ch/shhut.ac.ir/chaji"])
+su_table.add_row(["Electromangnatism", "16 - 18", "https://www.skyroom.online/ch/shhut.ac.ir/mazraeh-fard"])
+mo_table.add_row(["Electrical Circuites", "12 - 14", "https://www.skyroom.online/ch/shhut.ac.ir/erfani"])
+mo_table.add_row(["Sport 1", "14 - 16", "https://www.skyroom.online/ch/shhut.ac.ir/darvakh"])
+mo_table.add_row(["Electrical Language", "18 - 20", "https://www.skyroom.online/ch/shhut.ac.ir/erfani"])
+tu_table.add_row(["Engineer Maths", "08 - 10", "https://www.skyroom.online/ch/shhut.ac.ir/chaji"])
+tu_table.add_row(["Statistic and Probability", "10 - 12", "https://www.skyroom.online/ch/shhut.ac.ir/shahsanaii"])
+tu_table.add_row(["Islamic Revolution", "14 - 16", "https://www.skyroom.online/ch/shhut.ac.ir/darvakh"])
+tu_table.add_row(["Electromangnatism", "16 - 18", "https://www.skyroom.online/ch/shhut.ac.ir/mazraeh-fard"])
+we_table.add_row(["Number Calculations", "08 - 10", "https://www.skyroom.online/ch/shhut.ac.ir/pouyan"])
+we_table.add_row(["Number Calculations", "10 - 12", "https://www.skyroom.online/ch/shhut.ac.ir/keramat"])
 
 
-print(myTable)
+print(">>> Saturday <<<")
+print(sa_table)
+print(">>> Sunday <<<")
+print(su_table)
+print(">>> Monday <<<")
+print(mo_table)
+print(">>> Thursday <<<")
+print(tu_table)
+print(">>> Wednsaday <<<")
+print(we_table)
