a653943e17344ee29745e99793d70bd14c5d1785
Amir Hossein Roohi <ahroohi1379@hotmail.com>
2022-06-01 19:58:37 +0430

    new polybar stuff

--- blocky/launch.sh
+++ blocky/launch.sh
@@ -5,11 +5,15 @@
 DIR="$HOME/.config/polybar/blocky"
 
 # Terminate already running bar instances
-killall -q polybar
-
+pkill polybar
+pkill xfce4-panel
+# killall -q polybar
+# killall -q xfce4-panel
 # Wait until the processes have been shut down
-while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
+# while pgrep -u $UID -x xfce4-panel >/dev/null; do sleep 1; done
+# while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
 # Launch the bar
-polybar -q right -c "$DIR"/config.ini &
-polybar -q left -c "$DIR"/config.ini &
+polybar -q main -c "$DIR"/config.ini &
+# polybar -q right -c "$DIR"/config.ini &
+# polybar -q left -c "$DIR"/config.ini &
